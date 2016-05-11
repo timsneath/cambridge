da29c8ae39fafb471b20e6fcf00103d0a67cd570
Tim Sneath <tim@sneath.org>
2016-05-11 00:41:44 -0700

    tidy up display code a little

--- EmulatorCore/display.cs
+++ EmulatorCore/display.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -31,44 +32,44 @@ namespace ProjectCambridge.EmulatorCore
             Bitmap = new WriteableBitmap(256, 192);
             pixelStream = Bitmap.PixelBuffer.AsStream();
 
-            var colors = new Dictionary<byte, Color>();
-            colors.Add(0x00, Color.FromArgb(0xFF, 0x00, 0x00, 0x00)); // black
-            colors.Add(0x01, Color.FromArgb(0xFF, 0x00, 0x00, 0xCD)); // blue
-            colors.Add(0x02, Color.FromArgb(0xFF, 0xCD, 0x00, 0x00)); // red
-            colors.Add(0x03, Color.FromArgb(0xFF, 0xCD, 0x00, 0xCD)); // magenta
-            colors.Add(0x04, Color.FromArgb(0xFF, 0x00, 0xCD, 0x00)); // green
-            colors.Add(0x05, Color.FromArgb(0xFF, 0x00, 0xCD, 0xCD)); // cyan
-            colors.Add(0x06, Color.FromArgb(0xFF, 0xCD, 0xCD, 0x00)); // yellow
-            colors.Add(0x07, Color.FromArgb(0xFF, 0xCD, 0xCD, 0xCD)); // gray
-            colors.Add(0x08, Color.FromArgb(0xFF, 0x00, 0x00, 0x00)); // black
-            colors.Add(0x09, Color.FromArgb(0xFF, 0x00, 0x00, 0xFF)); // bright blue
-            colors.Add(0x0A, Color.FromArgb(0xFF, 0xFF, 0x00, 0x00)); // bright red
-            colors.Add(0x0B, Color.FromArgb(0xFF, 0xFF, 0x00, 0xFF)); // bright magenta
-            colors.Add(0x0C, Color.FromArgb(0xFF, 0x00, 0xFF, 0x00)); // bright green
-            colors.Add(0x0D, Color.FromArgb(0xFF, 0x00, 0xFF, 0xFF)); // bright cyan
-            colors.Add(0x0E, Color.FromArgb(0xFF, 0xFF, 0xFF, 0x00)); // bright yellow
-            colors.Add(0x0F, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF)); // white
+            var colors = new Dictionary<byte, Color>()
+            {
+                { 0x00, Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }, // black
+                { 0x01, Color.FromArgb(0xFF, 0x00, 0x00, 0xCD) }, // blue
+                { 0x02, Color.FromArgb(0xFF, 0xCD, 0x00, 0x00) }, // red
+                { 0x03, Color.FromArgb(0xFF, 0xCD, 0x00, 0xCD) }, // magenta
+                { 0x04, Color.FromArgb(0xFF, 0x00, 0xCD, 0x00) }, // green
+                { 0x05, Color.FromArgb(0xFF, 0x00, 0xCD, 0xCD) }, // cyan
+                { 0x06, Color.FromArgb(0xFF, 0xCD, 0xCD, 0x00) }, // yellow
+                { 0x07, Color.FromArgb(0xFF, 0xCD, 0xCD, 0xCD) }, // gray
+                { 0x08, Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }, // black
+                { 0x09, Color.FromArgb(0xFF, 0x00, 0x00, 0xFF) }, // bright blue
+                { 0x0A, Color.FromArgb(0xFF, 0xFF, 0x00, 0x00) }, // bright red
+                { 0x0B, Color.FromArgb(0xFF, 0xFF, 0x00, 0xFF) }, // bright magenta
+                { 0x0C, Color.FromArgb(0xFF, 0x00, 0xFF, 0x00) }, // bright green
+                { 0x0D, Color.FromArgb(0xFF, 0x00, 0xFF, 0xFF) }, // bright cyan
+                { 0x0E, Color.FromArgb(0xFF, 0xFF, 0xFF, 0x00) }, // bright yellow
+                { 0x0F, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF) }  // white
+            };
 
             SpectrumColors = new ReadOnlyDictionary<byte, Color>(colors);
         }
 
         public void ShowTestImage()
         {
-            // TODO: Data bind this to the image control 
-
             var r = new Random();
 
             // draw some stuff
             int idx;
-            for (int y = 0; y < 192; y++)
+            for (var y = 0; y < 192; y++)
             {
-                for (int x = 0; x < 256; x++)
+                for (var x = 0; x <= 255; x++)
                 {
                     idx = 4 * (y * 256 + x);
-                    displayBuffer[idx++] = (byte)255; // blue
-                    displayBuffer[idx++] = (byte)x; // green
-                    displayBuffer[idx++] = (byte)y; // red
-                    displayBuffer[idx] = (byte)255; // alpha
+                    displayBuffer[idx++] = 0xFF; // blue
+                    displayBuffer[idx++] = (byte)x;    // green
+                    displayBuffer[idx++] = (byte)y;    // red
+                    displayBuffer[idx]   = 0xFF; // alpha
                 }
             }
 
@@ -90,6 +91,17 @@ namespace ProjectCambridge.EmulatorCore
                 {
                     idx = 4 * (y * 256 + (x * 8));
 
+                    // Screen address can be calculated as follows:
+                    //
+                    //  15 14 13 12 11 10  9  8 |  7  6  5  4  3  2  1  0
+                    //   0  1  0 Y7 Y6 Y2 Y1 Y0 | Y5 Y4 Y3 X4 X3 X2 X1 X0
+                    //
+                    // where Y is pixels from top of screen
+                    // and X is pixels / 8 from left of screen
+                    // 
+                    // each address has monochrome bitmap for 8 horizontal pixels 
+                    // where 1 = ink color and 0 = paper color
+
                     // transform (x, y) coordinates to appropriate memory location
                     var y7y6 = (y & 0xC0) >> 6;
                     var y5y4y3 = (y & 0x38) >> 3;
@@ -98,10 +110,9 @@ namespace ProjectCambridge.EmulatorCore
                     var lo = (y5y4y3 << 5) | x;
                     var addr = (hi << 8) + lo;
 
-                    if ((addr > 0x5800) || (addr < 0x4000))
-                            {
-                        throw new IndexOutOfRangeException("address");
-                    }
+                    Debug.Assert(addr >= 0x4000);
+                    Debug.Assert(addr < 0x5800);
+
                     // read in the 8 pixels of monochrome data
                     var pixel8 = memory.ReadByte((ushort)addr);
 
@@ -113,7 +124,6 @@ namespace ProjectCambridge.EmulatorCore
                     //    F  B  P2 P1 P0 I2 I1 I0
                     // 
                     //  for 8x8 cells starting at 0x5800 (array of 32 x 24)
-                    //  so (0, 64) = 32
                     var color = memory.ReadByte((ushort)(0x5800 + ((y / 8) * 32 + x)));
                     var paperColor = SpectrumColors[(byte)((color & 0x78) >> 3)]; // 0x78 = 01111000
                     var inkColorAsByte = (byte)((color & 0x07)); // 0x07 = 00000111

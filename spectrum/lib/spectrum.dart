85ee46df715c228ce8c6062bc12b0257dcf1d7cc
Tim Sneath <timsneath@google.com>
2018-01-01 22:11:41 -0800

    Restructure source into library and app

---
+++ spectrum/lib/spectrum.dart
@@ -0,0 +1,8 @@
+library spectrum;
+
+export 'src/display.dart';
+export 'src/keyboard.dart';
+export 'src/memory.dart';
+export 'src/spectrumcolor.dart';
+export 'src/utility.dart';
+export 'src/z80.dart';

718472c27a293782033432f1f3b32c49d31d51e4
Tim Sneath <timsneath@google.com>
2018-01-01 19:10:01 -0800

    very basic display-less test

---
+++ core/main.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'z80.dart';
+import 'memory.dart';
+
+void main() {
+  Memory memory = new Memory(true);
+  Z80 z80 = new Z80(memory);
+
+  var rom = new File('z80.rom').readAsBytesSync();
+  memory.load(0x0000, rom);
+
+  int instructionsExecuted = 0;
+
+  while(!z80.cpuSuspended) {
+    z80.executeNextInstruction();
+
+    if (instructionsExecuted++ % 0x1000 == 0)
+    {
+      print("Program Counter: ${z80.pc}");
+    }
+  }
+}

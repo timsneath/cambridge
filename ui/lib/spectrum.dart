30c950e7edc6796d2c52d94b4f46e6dfab3b901d
Tim Sneath <timsneath@google.com>
2018-01-02 18:40:55 -0800

    tidy up UI

--- ui/lib/spectrum.dart
+++ ui/lib/spectrum.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:image/image.dart' as Image2;
 import 'package:spectrum/spectrum.dart';
 
@@ -47,14 +48,6 @@ class _SpectrumState extends State<Spectrum> {
     }
   }
 
-  createSpectrumFrame() {
-    var frame = new Image2.Image.fromBytes(Display.Width, Display.Height, Display.imageBuffer(memory), Image2.Image.RGBA);
-
-    final jpg = Image2.encodeJpg(frame, quality: 100);
-    final img = new Image.memory(jpg);
-    displayFrame = img;
-  }
-
   loadTestScreenshot() async {
     ByteData screen = await rootBundle.load('assets/AticAtac.scr');
     setState(() {
@@ -63,24 +56,63 @@ class _SpectrumState extends State<Spectrum> {
     });
   }
 
+  resetEmulator() async {
+    ByteData rom = await rootBundle.load('roms/48.rom');
+
+    setState(() {
+      memory = new Memory(true);
+      z80 = new Z80(memory, startAddress: 0x0000);
+      memory.load(0x0000, rom.buffer.asUint8List());
+      displayFrame = new Image.asset('assets/blank.jpg');
+    });
+  }
+
+  createSpectrumFrame() {
+    var frame = new Image2.Image.fromBytes(Display.Width, Display.Height,
+        Display.imageBuffer(memory), Image2.Image.RGBA);
+
+    final jpg = Image2.encodeJpg(frame, quality: 100);
+    final img = new Image.memory(jpg);
+    displayFrame = img;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.arrow_forward),
-        onPressed: loadTestScreenshot,
-      ),
       body: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            displayFrame,
+            displayFrame, // the Spectrum screen itself
             new Text(z80 == null
                 ? 'null'
-                : toHex16(z80.pc)) // yeah - we're wired up :)
+                : 'Program Counter: ${toHex16(z80.pc)}'), // yeah - we're wired up :)
+            new Container(
+                padding: const EdgeInsets.all(40.0),
+                child: new RichText(
+                    text: new TextSpan(
+                        text:
+                            "Not yet updating the screen frame-by-frame. Use the last button below to manually advance the emulator. You'll need to press it about 10 times to fully advance through the boot process.",
+                        style: new TextStyle(color: Colors.black87)))),
+            new ButtonTheme.bar(
+                child: new ButtonBar(
+              children: <Widget>[
+                new FlatButton(
+                    // padding: const EdgeInsets.all(4.0),
+                    child: new Text('Test screen'),
+                    onPressed: loadTestScreenshot),
+                new FlatButton(
+                    child: new Text('Reset'), onPressed: resetEmulator),
+                new FlatButton(
+                    // padding: const EdgeInsets.all(4.0),
+                    child: new Text('Step forward 65536 instructions'),
+                    onPressed: executeBatch)
+              ],
+            ))
           ],
         ),
       ),

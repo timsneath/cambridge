853ef84cc0c67c83c5958285a0880d5690d8ad08
Tim Sneath <timsneath@google.com>
2019-05-23 17:26:13 -0700

    Set default parameters for dasm CLI utility

--- dasm/dasm.dart
+++ dasm/dasm.dart
@@ -3,14 +3,19 @@ import 'dart:io';
 import '../spectrum/lib/spectrum/opcodes.dart';
 import '../spectrum/lib/spectrum/utility.dart';
 
-main(List<String> arguments) {
+void main(List<String> args) {
+  if (args.length == 0) {
+    args.add("0x0041");
+    args.add("0x0053");
+  }
+
   final file = new File('../spectrum/roms/48.rom');
   file.open(mode: FileMode.read);
 
   final rom = file.readAsBytesSync();
 
-  final start = int.parse(arguments[0]);
-  final end = int.parse(arguments[1]);
+  final start = int.parse(args[0]);
+  final end = int.parse(args[1]);
 
   int pc = start;
 

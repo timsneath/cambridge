440261b295c0dc5128b3526d16edc62cd08262fa
Tim Sneath <tim@sneath.org>
2020-12-29 17:05:35 -0800

    Refactor storage

---
+++ lib/core/storage.dart
@@ -0,0 +1,99 @@
+// storage.dart -- handles load/save of data
+
+import 'dart:typed_data';
+
+import 'ula.dart';
+import 'utility.dart';
+import 'z80.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class Storage {
+  final Z80 z80;
+
+  const Storage({required this.z80});
+
+  // Per https://faqwiki.zxnet.co.uk/wiki/SNA_format
+  // the snapshot format has a 27 byte header containing the Z80 registers
+  Future<void> loadSNASnapshot(ByteData snapshot) async {
+    final r = snapshot.buffer.asUint8List(0, 27);
+
+    z80.i = r[0];
+    z80.hl_ = createWord(r[1], r[2]);
+    z80.de_ = createWord(r[3], r[4]);
+    z80.bc_ = createWord(r[5], r[6]);
+    z80.af_ = createWord(r[7], r[8]);
+    z80.hl = createWord(r[9], r[10]);
+    z80.de = createWord(r[11], r[12]);
+    z80.bc = createWord(r[13], r[14]);
+    z80.iy = createWord(r[15], r[16]);
+    z80.ix = createWord(r[17], r[18]);
+    z80.iff2 = z80.iff1 = isBitSet(r[19], 2);
+    z80.r = r[20];
+    z80.af = createWord(r[21], r[22]);
+    z80.sp = createWord(r[23], r[24]);
+    z80.im = r[25];
+    ULA.screenBorder = r[26];
+
+    z80.memory.load(0x4000, snapshot.buffer.asUint8List(27));
+
+    // The program counter is pushed onto the stack, and since SP points to
+    // the stack, we can simply POP it off.
+    z80.pc = z80.POP();
+  }
+
+  // Per http://rk.nvg.ntnu.no/sinclair/formats/z80-format.html
+  // the snapshot format has a 30 byte header containing the Z80 registers
+  Future<void> loadZ80Snapshot(ByteData snapshot) async {
+    final r = snapshot.buffer.asUint8List(0, 30);
+
+    var fileFormatVersion = 145;
+    var isDataBlockCompressed = false;
+
+    if (createWord(r[6], r[7]) == 0) {
+      if (createWord(r[30], r[31]) == 54) {
+        fileFormatVersion = 300;
+      } else if (createWord(r[30], r[31]) == 23) {
+        fileFormatVersion = 201;
+      } else {
+        throw Exception('Unrecognized Z80 file format.');
+      }
+    }
+
+    z80.af = createWord(r[0], r[1]);
+    z80.bc = createWord(r[2], r[3]);
+    z80.hl = createWord(r[4], r[5]);
+    if (fileFormatVersion <= 145) {
+      z80.pc = createWord(r[6], r[7]);
+    } else {
+      z80.pc = createWord(r[32], r[33]);
+    }
+    z80.sp = createWord(r[8], r[9]);
+    z80.i = r[10];
+    z80.r = r[11];
+    ULA.screenBorder = (r[12] << 1) & 0x03;
+    if (isBitSet(r[12], 5)) {
+      isDataBlockCompressed = true;
+    }
+    z80.de = createWord(r[13], r[14]);
+    z80.bc_ = createWord(r[15], r[16]);
+    z80.de_ = createWord(r[17], r[18]);
+    z80.hl_ = createWord(r[19], r[20]);
+    z80.af_ = createWord(r[21], r[22]);
+    z80.iy = createWord(r[23], r[24]);
+    z80.ix = createWord(r[25], r[26]);
+    z80.iff1 = z80.iff2 = r[27] == 0;
+    if ((r[29] & 0x03) == 0x00) {
+      z80.im = 0;
+    } else if ((r[29] & 0x03) == 0x01) {
+      z80.im = 1;
+    } else if ((r[29] & 0x03) == 0x02) {
+      z80.im = 2;
+    }
+
+    if (!isDataBlockCompressed && (fileFormatVersion == 145)) {
+      z80.memory.load(0x4000, snapshot.buffer.asUint8List(30));
+    } else {
+      throw Exception('Unsupported -- compressed v1.45 Z80 file.');
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 
 import 'core/display.dart';
 import 'core/memory.dart';
+import 'core/storage.dart';
 import 'core/ula.dart';
 import 'core/utility.dart';
 import 'core/z80.dart';
@@ -42,6 +43,7 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
 
   bool isRomLoaded = false;
   bool isKeyboardVisible = false;
+  bool isDisassemblerVisible = false;
 
   @override
   void initState() {
@@ -57,7 +59,7 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
   }
 
   Future<void> onTick(Duration elapsed) async {
-    print('Tick: ${elapsed.inMilliseconds} ms.');
+    // print('Tick: ${elapsed.inMilliseconds} ms.');
     await executeFrame();
   }
 
@@ -70,94 +72,20 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
   }
 
   Future<void> loadSNASnapshot() async {
-    // Per https://faqwiki.zxnet.co.uk/wiki/SNA_format
-    // the snapshot format has a 27 byte header containing the Z80 registers
+    final storage = Storage(z80: z80);
     final snapshot = await rootBundle.load('roms/TIME.SNA');
-    final r = snapshot.buffer.asUint8List(0, 27);
-
-    z80.i = r[0];
-    z80.hl_ = createWord(r[1], r[2]);
-    z80.de_ = createWord(r[3], r[4]);
-    z80.bc_ = createWord(r[5], r[6]);
-    z80.af_ = createWord(r[7], r[8]);
-    z80.hl = createWord(r[9], r[10]);
-    z80.de = createWord(r[11], r[12]);
-    z80.bc = createWord(r[13], r[14]);
-    z80.iy = createWord(r[15], r[16]);
-    z80.ix = createWord(r[17], r[18]);
-    z80.iff2 = z80.iff1 = isBitSet(r[19], 2);
-    z80.r = r[20];
-    z80.af = createWord(r[21], r[22]);
-    z80.sp = createWord(r[23], r[24]);
-    z80.im = r[25];
-    ULA.screenBorder = r[26];
 
     setState(() {
-      memory.load(0x4000, snapshot.buffer.asUint8List(27));
-
-      // The program counter is pushed onto the stack, and since SP points to
-      // the stack, we can simply POP it off.
-      z80.pc = z80.POP();
+      storage.loadSNASnapshot(snapshot);
     });
   }
 
   Future<void> loadZ80Snapshot() async {
-    // Per http://rk.nvg.ntnu.no/sinclair/formats/z80-format.html
-    // the snapshot format has a 30 byte header containing the Z80 registers
-
+    final storage = Storage(z80: z80);
     final snapshot = await rootBundle.load('roms/JETSET.Z80');
-    final r = snapshot.buffer.asUint8List(0, 30);
-
-    var fileFormatVersion = 145;
-    var isDataBlockCompressed = false;
-
-    if (createWord(r[6], r[7]) == 0) {
-      if (createWord(r[30], r[31]) == 54) {
-        fileFormatVersion = 300;
-      } else if (createWord(r[30], r[31]) == 23) {
-        fileFormatVersion = 201;
-      } else {
-        throw Exception('Unrecognized Z80 file format.');
-      }
-    }
-
-    z80.af = createWord(r[0], r[1]);
-    z80.bc = createWord(r[2], r[3]);
-    z80.hl = createWord(r[4], r[5]);
-    if (fileFormatVersion <= 145) {
-      z80.pc = createWord(r[6], r[7]);
-    } else {
-      z80.pc = createWord(r[32], r[33]);
-    }
-    z80.sp = createWord(r[8], r[9]);
-    z80.i = r[10];
-    z80.r = r[11];
-    ULA.screenBorder = (r[12] << 1) & 0x03;
-    if (isBitSet(r[12], 5)) {
-      isDataBlockCompressed = true;
-    }
-    z80.de = createWord(r[13], r[14]);
-    z80.bc_ = createWord(r[15], r[16]);
-    z80.de_ = createWord(r[17], r[18]);
-    z80.hl_ = createWord(r[19], r[20]);
-    z80.af_ = createWord(r[21], r[22]);
-    z80.iy = createWord(r[23], r[24]);
-    z80.ix = createWord(r[25], r[26]);
-    z80.iff1 = z80.iff2 = r[27] == 0;
-    if ((r[29] & 0x03) == 0x00) {
-      z80.im = 0;
-    } else if ((r[29] & 0x03) == 0x01) {
-      z80.im = 1;
-    } else if ((r[29] & 0x03) == 0x02) {
-      z80.im = 2;
-    }
 
     setState(() {
-      if (!isDataBlockCompressed && (fileFormatVersion == 145)) {
-        memory.load(0x4000, snapshot.buffer.asUint8List(30));
-      } else {
-        throw Exception('Unsupported -- compressed v1.45 Z80 file.');
-      }
+      storage.loadZ80Snapshot(snapshot);
     });
   }
 
@@ -175,17 +103,17 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
   }
 
   Future<void> executeFrame() async {
-    z80.interrupt();
-    while (z80.tStates < 14336) {
+    final startTime = DateTime.now().millisecondsSinceEpoch;
+    setState(() {
+      z80.interrupt();
+    });
+    while (DateTime.now().millisecondsSinceEpoch - startTime < 10) {
       z80.executeNextInstruction();
       if (breakpoints.contains(z80.pc) && ticker.isActive) {
         ticker.stop();
         break;
       }
     }
-    setState(() {
-      z80.tStates = 0;
-    });
   }
 
   void stepInstruction() {
@@ -197,6 +125,12 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
   void keyboardToggleVisibility() {
     setState(() {
       isKeyboardVisible = !isKeyboardVisible;
+    });
+  }
+
+  void disassemblerToggleVisibility() {
+    setState(() {
+      isDisassemblerVisible = !isDisassemblerVisible;
     });
   }
 
@@ -217,10 +151,9 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
               onPressed: resetEmulator,
             ),
             IconButton(
-              icon: const Icon(Icons.file_download),
-              tooltip: 'Load tape snapshot',
-              onPressed: loadZ80Snapshot,
-            ),
+                icon: const Icon(Icons.file_download),
+                tooltip: 'Load tape snapshot',
+                onPressed: loadZ80Snapshot),
             IconButton(
               icon: ticker.isActive
                   ? const Icon(
@@ -244,6 +177,11 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
               tooltip: 'Show/hide keyboard',
               onPressed: keyboardToggleVisibility,
             ),
+            IconButton(
+              icon: const Icon(Icons.bug_report),
+              tooltip: 'Show/hide disassembler',
+              onPressed: disassemblerToggleVisibility,
+            ),
           ],
         ),
       ],
@@ -253,10 +191,8 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
   void toggleTicker() {
     setState(() {
       if (ticker.isActive) {
-        print('Ticker stopping...');
         ticker.stop();
       } else {
-        print('Ticker starting...');
         ticker.start();
       }
     });
@@ -273,7 +209,7 @@ class CambridgeHomePageState extends State<CambridgeHomePage> {
           Monitor(memory: memory),
           Text('Program Counter: ${toHex16(z80.pc)}'),
           menus(),
-          DisassemblyView(),
+          if (isDisassemblerVisible) DisassemblyView(),
           if (isKeyboardVisible) Keyboard(),
         ],
       ),
